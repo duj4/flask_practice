24c67afe5be63bd4069909308a82a37759429431
duj4 <boyu.du@yahoo.com>
2019-12-08 16:45:09 +0800

    3.1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, abort, make_response, jsonify, session
+from flask import Flask, url_for, request, redirect, abort, make_response, jsonify, session, render_template
 from urllib.parse import urlparse, urljoin
 from jinja2.utils import generate_lorem_ipsum
 import json
@@ -237,6 +237,28 @@ def logout():
         session.pop('logged_in')
 
     return redirect(url_for('hello'))
+
+#chapter 3-template
+user = {
+    'username': 'Grey Li',
+    'bio': 'A boy who loves movies and music.'
+}
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'}
+]
+
+@app.route('/watchlist')
+def watchlist():
+    return render_template('watchlist.html', user=user, movies=movies)
 
 if __name__ == '__main__':
     app.run(debug = app.config['DEBUG'],
