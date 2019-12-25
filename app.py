5229afdcdb7f9fb3c116665ee4907777cea1bb64
duj4 <boyu.du@yahoo.com>
2019-12-25 15:29:12 +0800

    5.6.1

--- app.py
+++ app.py
@@ -541,6 +541,17 @@ class EditNoteForm(FlaskForm):
 class DeleteNoteForm(FlaskForm):
     submit = SubmitField('Delete')
 
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        click.confirm("This operation will delete the database, do you want to continue?", abort=True)
+        db.drop_all()
+        click.echo("Drop tables successfully.")
+    db.create_all()
+    click.echo("Initialized database successfully.")
+
 @app.route('/index_DB')
 def index_DB():
     form = DeleteNoteForm()
